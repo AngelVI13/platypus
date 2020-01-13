080d24316b3c76982ae45b508231d623c938e49a
Angel ILIEV <a.v.iliev13@gmail.com>
2020-01-13 09:36:10 +0200

    Added en passant move generation

--- board/moves.go
+++ board/moves.go
@@ -43,6 +43,19 @@ const QueenSide uint64 = 0b00001111_00001111_00001111_00001111_00001111_00001111
 // KingSide Bitmask for selecting all kingside squares
 const KingSide uint64 = 0b11110000_11110000_11110000_11110000_11110000_11110000_11110000_11110000
 
+// FileMasks8 Array that holds bitmasks that select a given file based on the index of
+// the element i.e. index 0 selects File A, 1- FileB etc.
+var FileMasks8 [8]uint64 = [8]uint64{
+	0b00000001_00000001_00000001_00000001_00000001_00000001_00000001_00000001,
+	0b00000010_00000010_00000010_00000010_00000010_00000010_00000010_00000010,
+	0b00000100_00000100_00000100_00000100_00000100_00000100_00000100_00000100,
+	0b00001000_00001000_00001000_00001000_00001000_00001000_00001000_00001000,
+	0b00010000_00010000_00010000_00010000_00010000_00010000_00010000_00010000,
+	0b00100000_00100000_00100000_00100000_00100000_00100000_00100000_00100000,
+	0b01000000_01000000_01000000_01000000_01000000_01000000_01000000_01000000,
+	0b10000000_10000000_10000000_10000000_10000000_10000000_10000000_10000000,
+}
+
 // TODO This should be in a struct
 var NotWhitePieces uint64
 var BlackPieces uint64
@@ -66,6 +79,7 @@ func DrawBitboard(bitboard uint64) {
 	for _, rank := range bitboardStr {
 		fmt.Printf("%s\n", rank)
 	}
+	fmt.Println()
 }
 
 func (board *Board) PossibleMovesWhite(history string) (moveList string) {
@@ -107,6 +121,7 @@ func (board *Board) PossibleMovesWhite(history string) (moveList string) {
 	fmt.Println()
 
 	moveList = board.PossiblePawnMovesWhite(history)
+	fmt.Println(moveList)
 	return moveList
 }
 
@@ -114,7 +129,10 @@ func (board *Board) PossiblePawnMovesWhite(history string) (moveList string) {
 	// todo movelist as string looks pretty stupid ???
 
 	wp := board.bitboards[WP]
+	var possibility uint64  // holds one potential capture at a time
+	var index int  // index of the "possibility" capture
 	var pawnMoves uint64
+
 	// Move all bits from the white pawn bitboard to the left by 7 squares
 	// and disable File A (we do not want any leftovers from the move to the left)
 	// also remove any captures on the 8th rank since that will be handled by promotions
@@ -155,58 +173,120 @@ func (board *Board) PossiblePawnMovesWhite(history string) (moveList string) {
 	// [               ]
 	// [               ]
 	// [               ]
+	// captures and moves formward forward: x1,y1,x2,y2
 	pawnMoves = (wp >> 7) & (BlackPieces) & (^Rank8) & (^FileA) // capture right
-	// Only iterate from the first occurance of a '1' to the last occurance of a '1' in the bitboard
-	for i := bits.TrailingZeros64(pawnMoves); i < 64-bits.LeadingZeros64(pawnMoves); i++ {
-		if ((pawnMoves >> i) & 1) == 1 {
-			//                                  final_rank final_file start_r  start_f
-			moveList += fmt.Sprintf("%d%d%d%d ", (i/8 + 1), (i%8 - 1), (i / 8), (i % 8))
-		}
+	// Find first bit which is equal to '1' i.e. first capture
+	possibility = pawnMoves & (^(pawnMoves -1 ))
+	for possibility != 0 {
+		index = bits.TrailingZeros64(possibility)
+		//                                   final_rank     final_file     start_rank   start_file
+		moveList += fmt.Sprintf("%d%d%d%d ", (index/8 + 1), (index%8 - 1), (index / 8), (index % 8))
+		pawnMoves &= ^possibility  // remove the capture that we just analyzed
+		possibility = pawnMoves & (^(pawnMoves -1 ))  // find next bit equal to '1' i.e. next capture
 	}
 
 	pawnMoves = (wp >> 9) & (BlackPieces) & (^Rank8) & (^FileH) // capture left
-	for i := bits.TrailingZeros64(pawnMoves); i < 64-bits.LeadingZeros64(pawnMoves); i++ {
-		if ((pawnMoves >> i) & 1) == 1 {
-			moveList += fmt.Sprintf("%d%d%d%d ", (i/8 + 1), (i%8 + 1), (i / 8), (i % 8))
-		}
+	possibility = pawnMoves & (^(pawnMoves -1 ))
+	for possibility != 0 {
+		index = bits.TrailingZeros64(possibility)
+		//                                   final_rank     final_file     start_rank   start_file
+		moveList += fmt.Sprintf("%d%d%d%d ", (index/8 + 1), (index%8 + 1), (index / 8), (index % 8))
+		pawnMoves &= ^possibility  // remove the capture that we just analyzed
+		possibility = pawnMoves & (^(pawnMoves -1 ))  // find next bit equal to '1' i.e. next capture
 	}
 
 	pawnMoves = (wp >> 8) & Empty & (^Rank8) // move 1 square forward
-	for i := bits.TrailingZeros64(pawnMoves); i < 64-bits.LeadingZeros64(pawnMoves); i++ {
-		if ((pawnMoves >> i) & 1) == 1 {
-			moveList += fmt.Sprintf("%d%d%d%d ", (i/8 + 1), (i % 8), (i / 8), (i % 8))
-		}
+	possibility = pawnMoves & (^(pawnMoves -1 ))
+	for possibility != 0 {
+		index = bits.TrailingZeros64(possibility)
+		//                                   final_rank     final_file start_rank   start_file
+		moveList += fmt.Sprintf("%d%d%d%d ", (index/8 + 1), (index%8), (index / 8), (index % 8))
+		pawnMoves &= ^possibility  // remove the capture that we just analyzed
+		possibility = pawnMoves & (^(pawnMoves -1 ))  // find next bit equal to '1' i.e. next capture
 	}
 
-	// Move all pawns 2 ranks, check that, in between and on the final square thre is nothing,
+	// Move all pawns 2 ranks, check that, in between and on the final square there is nothing,
 	// also check that resulting square is on rank4.
 	// (instead of check I mean eliminate squares that do not comply with these conditions)
 	pawnMoves = (wp >> 16) & Empty & (Empty >> 8) & Rank4 // move 2 squares forward
-	for i := bits.TrailingZeros64(pawnMoves); i < 64-bits.LeadingZeros64(pawnMoves); i++ {
-		if ((pawnMoves >> i) & 1) == 1 {
-			moveList += fmt.Sprintf("%d%d%d%d ", (i/8 + 2), (i % 8), (i / 8), (i % 8))
-		}
+	possibility = pawnMoves & (^(pawnMoves -1 ))
+	for possibility != 0 {
+		index = bits.TrailingZeros64(possibility)
+		//                                   final_rank     final_file start_rank   start_file
+		moveList += fmt.Sprintf("%d%d%d%d ", (index/8 + 2), (index%8), (index / 8), (index % 8))
+		pawnMoves &= ^possibility  // remove the capture that we just analyzed
+		possibility = pawnMoves & (^(pawnMoves -1 ))  // find next bit equal to '1' i.e. next capture
 	}
 
-	// promotions
+	// promotions - format: y1,y2,Promotion Type,"P"
 	pawnMoves = (wp >> 7) & BlackPieces & Rank8 & (^FileA) // pawn promotion by capture right
-	for i := bits.TrailingZeros64(pawnMoves); i < 64-bits.LeadingZeros64(pawnMoves); i++ {
-		if ((pawnMoves >> i) & 1) == 1 {
-			moveList += fmt.Sprintf("%d%dQP %d%dRP %d%dBP %d%dNP ", (i/8 - 1), (i % 8), (i/8 - 1), (i % 8), (i/8 - 1), (i % 8), (i/8 - 1), (i % 8))
-		}
+	possibility = pawnMoves & (^(pawnMoves -1 ))
+	for possibility != 0 {
+		index = bits.TrailingZeros64(possibility)
+		moveList += fmt.Sprintf("%d%dQP %d%dRP %d%dBP %d%dNP ",
+								(index/8 - 1), (index % 8),
+								(index/8 - 1), (index % 8),
+								(index/8 - 1), (index % 8),
+								(index/8 - 1), (index % 8))
+		pawnMoves &= ^possibility  // remove the capture that we just analyzed
+		possibility = pawnMoves & (^(pawnMoves -1 ))  // find next bit equal to '1' i.e. next capture
 	}
+
 	pawnMoves = (wp >> 9) & BlackPieces & Rank8 & (^FileH) // pawn promotion by capture left
-	for i := bits.TrailingZeros64(pawnMoves); i < 64-bits.LeadingZeros64(pawnMoves); i++ {
-		if ((pawnMoves >> i) & 1) == 1 {
-			moveList += fmt.Sprintf("%d%dQP %d%dRP %d%dBP %d%dNP ", (i/8 + 1), (i % 8), (i/8 + 1), (i % 8), (i/8 + 1), (i % 8), (i/8 + 1), (i % 8))
-		}
+	possibility = pawnMoves & (^(pawnMoves -1 ))
+	for possibility != 0 {
+		index = bits.TrailingZeros64(possibility)
+		moveList += fmt.Sprintf("%d%dQP %d%dRP %d%dBP %d%dNP ",
+								(index/8 + 1), (index % 8),
+								(index/8 + 1), (index % 8),
+								(index/8 + 1), (index % 8),
+								(index/8 + 1), (index % 8))
+		pawnMoves &= ^possibility  // remove the capture that we just analyzed
+		possibility = pawnMoves & (^(pawnMoves -1 ))  // find next bit equal to '1' i.e. next capture
 	}
+
 	pawnMoves = (wp >> 8) & Empty & Rank8 // pawn promotion by move 1 forward
-	for i := bits.TrailingZeros64(pawnMoves); i < 64-bits.LeadingZeros64(pawnMoves); i++ {
-		if ((pawnMoves >> i) & 1) == 1 {
-			moveList += fmt.Sprintf("%d%dQP %d%dRP %d%dBP %d%dNP ", (i / 8), (i % 8), (i / 8), (i % 8), (i / 8), (i % 8), (i / 8), (i % 8))
+	possibility = pawnMoves & (^(pawnMoves -1 ))
+	for possibility != 0 {
+		index = bits.TrailingZeros64(possibility)
+		moveList += fmt.Sprintf("%d%dQP %d%dRP %d%dBP %d%dNP ",
+								(index/8), (index % 8),
+								(index/8), (index % 8),
+								(index/8), (index % 8),
+								(index/8), (index % 8))
+		pawnMoves &= ^possibility  // remove the capture that we just analyzed
+		possibility = pawnMoves & (^(pawnMoves -1 ))  // find next bit equal to '1' i.e. next capture
+	}
+
+	if histLen := len(history); histLen >= 4 {
+		// if history move, moved 2 squares forward and inside the same file
+		if (history[histLen - 1] == history[histLen - 3]) &&
+			(abs(int(history[histLen-2] - history[histLen-4])) == 2) {
+			enPassFile := history[histLen -1] - "0"[0]  // find enpass file (type byte)
+			// en passant right
+			// shows piece to remove, not the destination
+			possibility = (wp << 1) & board.bitboards[BP] & Rank5 & (^FileA) & FileMasks8[enPassFile]
+			if possibility != 0 {
+				index = bits.TrailingZeros64(possibility)
+				moveList += fmt.Sprintf("%d%d E", (index%8 -1), (index%8))
+			}
+
+			// en passant left
+			// shows piece to remove, not the destination
+			possibility = (wp >> 1) & board.bitboards[BP] & Rank5 & (^FileH) & FileMasks8[enPassFile]
+			if possibility != 0 {
+				index = bits.TrailingZeros64(possibility)
+				moveList += fmt.Sprintf("%d%d E", (index%8 +1), (index%8))
+			}
 		}
 	}
 
 	return moveList
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
--- board/moves_test.go
+++ board/moves_test.go
@@ -1,11 +1,26 @@
 package board
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+	"strings"
+)
 
 func TestPossibleMovesWhite(t *testing.T) {
 	board := Board{}
-	board.ParseStringArray(StartingPosition)
+	// board.ParseStringArray(StartingPosition)
+	board.ParseStringArray([8][8]string{
+		[8]string{"r", "n", "b", "q", "k", "b", "n", "r"},
+		[8]string{"p", "p", "p", "p", "p", "p", " ", "p"},
+		[8]string{" ", " ", " ", " ", " ", " ", " ", " "},
+		[8]string{" ", " ", " ", " ", " ", " ", "p", "P"},
+		[8]string{" ", " ", " ", " ", " ", " ", " ", " "},
+		[8]string{" ", " ", " ", " ", " ", " ", " ", " "},
+		[8]string{"P", "P", "P", "P", "P", "P", "P", "P"},
+		[8]string{"R", "N", "B", "Q", "K", "B", "N", "R"}})
 
-	board.PossibleMovesWhite("")
+
+	moveList := board.PossibleMovesWhite("3656")
+	fmt.Println(len(strings.Split(moveList, " ")))
 	t.Errorf("Error")
 }
