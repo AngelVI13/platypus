ec4d11581ba1c6664ed47e8a1820c5639ddd04a7
Angel ILIEV <a.v.iliev13@gmail.com>
2020-01-16 09:16:23 +0200

    Added performance timinig for move gen

--- platypus.go
+++ platypus.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"platypus/board"
+	"time"
 )
 
 func main() {
 	boardVar := board.Board{}
 	boardVar.ParseStringArray(board.StartingPosition)
 	fmt.Println(&boardVar)
-	boardVar.PrintBitboards()
+
+	// todo refactor moves instead of string as an array or slice ?
+	// todo right now loosing a lot of speed from this
+	start := time.Now()
+	for i := 0; i < 1000; i++ {
+		boardVar.PossibleMovesWhite()
+	}
+	end := time.Since(start)
+	fmt.Printf("MoveGen (1000): %s\n", end)
 }
