dceb736197b478143f3e663175faa333d5f0230e
Angel Iliev <angel@localhost.localdomain>
2020-01-26 08:29:59 +0200

    Added benchmark for perft 5 from startpos

--- board/perft_test.go
+++ board/perft_test.go
@@ -17,3 +17,17 @@ func TestPerftStartingPosition(t *testing.T) {
 	fmt.Println(PerftPromotions)
 	t.Errorf("Errror\n")
 }
+
+func BenchmarkPerftStartingPositionDepth5(b *testing.B) {
+	board := Board{}
+	board.ParseFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+
+	PerftMaxDepth = 5
+
+	// This performs ~54% faster than hugo
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Perft(board, 0)
+	}
+	b.StopTimer()
+}
