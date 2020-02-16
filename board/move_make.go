43089bd35404bb26de24af403f6319a86a4f1804
Angel Iliev <angel@localhost.localdomain>
2020-02-16 20:47:26 +0200

    Fixed castling bug. Added position updating during make move. Used new Captured() to handle captures.

--- board/move_make.go
+++ board/move_make.go
@@ -5,13 +5,11 @@ import (
 )
 
 func (board *Board) removePieceFromSq(pieceType, sq int) {
-	board.position[sq] = NoPiece
 	board.bitboards[pieceType] &= (^(1 << sq))
 	board.positionKey ^= PieceKeys[pieceType][sq]
 }
 
 func (board *Board) addPieceToSq(pieceType, sq int) {
-	board.position[sq] = pieceType
 	board.bitboards[pieceType] |= 1 << sq
 	board.positionKey ^= PieceKeys[pieceType][sq]
 }
@@ -21,6 +19,7 @@ func (board *Board) MakeMove(move int) {
 	fromSq := FromSq(move)
 	toSq := ToSq(move)
 	pieceType := board.position[fromSq]
+	capturedPiece := board.position[toSq]
 
 	// Store has value before we do any hashing in/out of pieces etc
 	board.history[board.ply].positionKey = board.positionKey
@@ -29,8 +28,10 @@ func (board *Board) MakeMove(move int) {
 
 	// Remove piece from start sq in piece's bitboard
 	board.removePieceFromSq(pieceType, fromSq)
+	board.position[fromSq] = NoPiece
 	// Add the piece to end sq in piece's bitboard
 	board.addPieceToSq(pieceType, toSq)
+	board.position[toSq] = pieceType
 
 	if CastleFlag(move) == 1 {
 		// PerftCastles++
@@ -38,54 +39,46 @@ func (board *Board) MakeMove(move int) {
 		if pieceType == WK && toSq == G1 {
 			board.removePieceFromSq(WR, H1)
 			board.addPieceToSq(WR, F1)
+			board.position[H1] = NoPiece
+			board.position[F1] = WR
 		} else if pieceType == WK && toSq == C1 {
 			board.removePieceFromSq(WR, A1)
 			board.addPieceToSq(WR, D1)
+			board.position[A1] = NoPiece
+			board.position[D1] = WR
 		} else if pieceType == BK && toSq == G8 {
 			board.removePieceFromSq(WR, H8)
 			board.addPieceToSq(WR, F8)
-		} else if pieceType == WK && toSq == C8 {
-			board.removePieceFromSq(WR, A8)
-			board.addPieceToSq(WR, D8)
+			board.position[H8] = NoPiece
+			board.position[F8] = BR
+		} else if pieceType == BK && toSq == C8 {
+			board.removePieceFromSq(BR, A8)
+			board.addPieceToSq(BR, D8)
+			board.position[A8] = NoPiece
+			board.position[D8] = BR
 		} else {
 			panic("Incorrect castle move")
 		}
 	}
 
-	// todo might have to update EnemyPieces here, otherwise we rely that somebody called GetMoves before that
-	// todo Or store this bitboard in history and in current board
-	// if ToSq is occupied by one of enemy's pieces -> it was a capture
-	if (board.stateBoards[EnemyPieces]>>toSq)&1 == 1 {
+	// capturedPiece := Captured(move)
+	if capturedPiece != NoPiece && EnPassantFlag(move) == 0 {
 		board.fiftyMove = 0 // reset 50 move rule counter
-		// PerftCaptures++
 
-		// todo this should be done only once for the current board state NOT on every makemove
-		var startRange int
-		var endRange int
-		if board.Side == White {
-			// if current side is white the we capture from blacks pieces
-			startRange = BP
-			endRange = BK
-		} else {
-			startRange = WP
-			endRange = WK
-		}
+		// note: it is already removed from board.position -> no need to remove it here
+		board.removePieceFromSq(capturedPiece, toSq) // remove enemy piece from its board
+	} else if (capturedPiece == WP || capturedPiece == BP) && EnPassantFlag(move) == 1 {
+		board.fiftyMove = 0 // reset 50 move rule counter
 
-		for i := startRange; i <= endRange; i++ {
-			// if destination square is on the board -> must be the correct board for the capture
-			if (board.bitboards[i]>>toSq)&1 == 1 {
-				// todo update material here
-				board.removePieceFromSq(i, toSq) // remove enemy piece from its board
-				break
-			}
-		}
-	} else if ((board.stateBoards[Empty]>>toSq)&1 == 1) && (EnPassantFlag(move) == 1) {
 		// PerftEnPassant++
 		// Otherwise if destination piece is empty but the move is enpassant -> remove captured piece
 		if board.Side == White {
 			board.removePieceFromSq(BP, toSq+8)
+			// note: need to remove capture enpassant pawn since it is not in fromSq or toSq
+			board.position[toSq+8] = NoPiece
 		} else {
 			board.removePieceFromSq(WP, toSq-8)
+			board.position[toSq-8] = NoPiece
 		}
 	}
 
@@ -122,6 +115,7 @@ func (board *Board) MakeMove(move int) {
 		// PerftPromotions++
 		// todo update material here
 		board.addPieceToSq(promoted, toSq)
+		board.position[toSq] = promoted
 		// we already move the pawn to the 8th rank and since it is a promotion
 		// not only we add the promoted piece but we need to remove the pawn
 		// from the 8th rank
