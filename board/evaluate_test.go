cd2cdefdaa30ca70c149cd0bf12918454611cb54
Angel ILIEV <a.v.iliev13@gmail.com>
2021-01-22 15:26:36 +0200

    Updated eval tests

--- board/evaluate_test.go
+++ board/evaluate_test.go
@@ -2,7 +2,6 @@ package board
 
 import (
 	"testing"
-	"fmt"
 )
 
 
@@ -58,40 +57,39 @@ func TestMirrorBoard(t *testing.T) {
 	// Expect that material for both black and white will be equal
 	
 	InitHashKeys()
-	board := Board{}
-	// board.ParseFen(StartingPosition)
-	board.ParseFen("rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2")
-	
-	whiteMaterial := board.material[White]
-	blackMaterial := board.material[Black]
-	positionKey := board.positionKey
 
-	fmt.Println(&board)
-
-	MirrorBoard(&board)
-
-	fmt.Println(&board)
-
-	if whiteMaterial != board.material[White] {
-		t.Errorf(
-			"Incorrect material for white startpos after mirror: Initial: %d != Mirror %d\n",
-			whiteMaterial, board.material[White],
-		)
+	positions := []string{
+		StartingPosition, 
+		"rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2",
+		"r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+		"3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1",
+		"8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1",
+		"rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
 	}
 
-	if blackMaterial != board.material[Black] {
-		t.Errorf(
-			"Incorrect material for black startpos after mirror: Initial: %d != Mirror %d\n",
-			blackMaterial, board.material[Black],
-		)
-	}
+	for idx, position := range positions {
+		board := Board{}
+		board.ParseFen(position)
+		
+		whiteMaterial := board.material[White]
+		blackMaterial := board.material[Black]
 
-	fmt.Println(board.positionKey)
-	if positionKey != board.positionKey {
-		t.Errorf(
-			"Incorrect positionKey for startpos after mirror: Initial: %d != Mirror %d\n",
-			positionKey, board.positionKey,
-		)
+		MirrorBoard(&board)
+
+		// what was initially from whites side is now from blacks
+		if whiteMaterial != board.material[Black] {
+			t.Errorf(
+				"(Pos: %d) Incorrect material for white after mirror: Initial: %d != Mirror %d\n",
+				idx, whiteMaterial, board.material[Black],
+			)
+		}
+
+		if blackMaterial != board.material[White] {
+			t.Errorf(
+				"(Pos: %d) Incorrect material for black after mirror: Initial: %d != Mirror %d\n",
+				idx, blackMaterial, board.material[White],
+			)
+		}
 	}
 }
 
